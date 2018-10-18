1a9bf3f55b99d310fa8c90b6b11433d09a9a8e11
adgchending <13510865321@139.com>
2018-10-18 09:10:42 +0800

    2018 10 18

---
+++ src/main/java/com/cd/basic/api/SchoolController.java
@@ -0,0 +1,40 @@
+package com.cd.basic.api;
+
+import com.cd.basic.service.BasicSchoolInforService;
+import com.cd.common.util.Result;
+import com.cd.common.util.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/act")
+@Api(tags = "流程Demo", value = "流程接口")
+public class SchoolController {
+    @Autowired
+    BasicSchoolInforService service;
+
+    @ApiOperation(value = "一个测试")
+    @GetMapping("/ceshi")
+    //新增一个请假单
+    public Result addLeaveInfo(@Param(value = "请假事由") @RequestParam String id) {
+        switch (id) {
+            case "1":
+                return ResultUtil.success("你好1");
+            case "2":
+                return ResultUtil.success("你好2");
+            case "3":
+                return ResultUtil.success("你好3");
+            case "4":
+                return ResultUtil.success("你好4");
+            case "5":
+                return ResultUtil.success("你好6");
+
+            default:
+                break;
+        }
+        return null;
+    }
+}
