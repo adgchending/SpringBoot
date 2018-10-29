34679552889c33fe0f637bb574ae99200ed0ac74
adgchending <13510865321@139.com>
2018-10-29 17:17:19 +0800

    删除mapper.xml配置文件  没什么卵用

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -3,12 +3,15 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //开启事物
 @EnableTransactionManagement
 @ComponentScan
+//开启定时任务(让SpringBoot知道你在用定时器)
+@EnableScheduling
 public class App {
 
 	public static void main(String[] args) {
