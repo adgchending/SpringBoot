c518647861ac97ac12a405eb365ae7e7cc70ba26
adgchending <13510865321@139.com>
2018-11-02 18:26:09 +0800

    shiro

---
+++ src/main/java/com/cd/shiro/domamin/User.java
@@ -0,0 +1,42 @@
+package com.cd.shiro.domamin;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String name;
+    private Integer password;
+    private List<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
--- src/main/java/com/cd/sys_manage/LoginC.java
+++ src/main/java/com/cd/sys_manage/LoginC.java
@@ -4,10 +4,8 @@ import com.cd.common.util.Result;
 import com.cd.common.util.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,10 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "pc登录接口")
 public class LoginC {
 
-
     @ApiOperation(value = "登录接口")
-    @GetMapping("/login")
-    public Result selectSchoolInfor(@ApiParam(value = "请假事由") @RequestParam String id) {
+    @PostMapping("/login")
+    public Result selectSchoolInfor() {
+
+
         return ResultUtil.success();
     }
 }
