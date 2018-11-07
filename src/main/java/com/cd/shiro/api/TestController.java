d8b0bc6b10d3d834631cca4152f9f6778dbe32d0
adgchending <13510865321@139.com>
2018-11-07 18:18:47 +0800

    shiro

---
+++ src/main/java/com/cd/shiro/api/TestController.java
@@ -0,0 +1,34 @@
+package com.cd.shiro.api;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author sgl
+ * @Date 2018-05-08 13:55
+ */
+@RestController
+public class TestController {
+
+    /**
+     * 没有加shiro权限注解
+     * @return
+     */
+    @RequestMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+
+    /**
+     * 添加了shiro权限注解，需要用户有"systemUserAdd"权限
+     * @return
+     */
+    @RequestMapping("/test2")
+    @RequiresPermissions("systemUserAdd")
+    public String test2(){
+        return "test2";
+    }
+
+}
---
+++ src/main/java/com/cd/shiro/pojo/domain/SysUser.java
@@ -0,0 +1,52 @@
+package com.cd.shiro.pojo.domain;
+
+import java.io.Serializable;
+
+public class SysUser implements Serializable {
+    private Long userId;
+    private String userName;
+    private String fullName;
+    private String password;
+    private String salt;
+    private static final long serialVersionUID = 1L;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
