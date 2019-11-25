0e063bde915190c0532f86f7231d65bd0516db33
陈尚贤 <13510865321@139.com>
2019-11-25 16:59:25 +0800

    测试自定义properties

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,31 +1,21 @@
-import com.App;
+import com.cd.basic.pojo.bo.StudentBo;
 import com.cd.redis.util.RedisUtils;
-import com.cd.shiro.pojo.domain.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = App.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = App.class)
 public class test {
-    protected static Logger LOGGER = LoggerFactory.getLogger(test.class);
 
     @Autowired
     RedisTemplate redisTemplate;
@@ -34,9 +24,48 @@ public class test {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
-    @Test
-    public static void main(String[] args) {
-        System.out.printf("");
+
+    public static void main(String[] args) throws SQLException {
+        List<StudentBo> bos = new ArrayList<>();
+        //新增10W条数据耗时13秒
+        for (int i = 0; i <100000 ; i++) {
+            String name = "张三"+i;
+            StudentBo studentBo = new StudentBo();
+            studentBo.setName(name);
+            studentBo.setAge(i);
+            bos.add(studentBo);
+        }
+
+
+        long start = System.currentTimeMillis();
+        System.out.println(start);
+        Connection conn = null;
+        String url  = "jdbc:mysql://127.0.0.1:3306/springboot?useUnicode=true&useSSL=false&characterEncoding=utf8";
+        String username = "root";
+        String password = "root";
+        conn = DriverManager.getConnection(url, username, password);
+        conn.setAutoCommit(false);
+        PreparedStatement ps = null;
+        String sql = "insert into thread VALUES (?,?)";
+        // 批量插入时ps对象必须放到for循环外面
+        ps = conn.prepareStatement(sql);
+        for (int i = 0; i < bos.size(); i++) {
+            ps.setString(1, bos.get(i).getName());
+            ps.setInt(2, bos.get(i).getAge());
+
+            ps.addBatch();
+            if (i % 1000 == 0) {
+                ps.executeBatch();
+                conn.commit();
+                ps.clearBatch();
+            }
+        }
+        // 剩余数量不足1000
+        ps.executeBatch();
+        conn.commit();
+        ps.clearBatch();
+        long end = System.currentTimeMillis();
+        System.out.println(((end - start) / 1000) + "秒");
     }
 
 
